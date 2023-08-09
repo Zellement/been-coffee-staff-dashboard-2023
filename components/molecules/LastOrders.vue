3a4e6fb304743eea24d157daeac8f2c41d714567
Dan Farrow <dan@zellement.com>
2023-08-09 10:01:36 +0100

    tidy up

--- components/molecules/LastOrders.vue
+++ components/molecules/LastOrders.vue
@@ -8,7 +8,7 @@
                 <div class="w-full py-8 overflow-x-scroll ">
                     <div class="flex flex-row w-full space-x-4">
                         <div class="flex relative flex-col w-3/4 p-4 shadow-lg rounded-xl md:w-auto min-w-[300px] sm:w-2/5 bg-seashell-400 dark:bg-navy-400" v-for="item in data.allOrders" :key="item.id">
-                            <div class="absolute top-0 right-0 uppercase -translate-y-1/2 bg-white text-3xs px-2 dark:text-navy-50 dark:bg-navy-400 text-gray-400 rounded-tl-lg py-0.5">{{  item.supplier.name }}</div>
+                            <div class="absolute top-0 right-0 uppercase -translate-y-1/2 bg-white text-3xs px-2 dark:text-navy-50 dark:bg-navy-400 text-gray-400 rounded-tl-lg py-0.5 tracking-wider">{{  item.supplier.name }}</div>
                             <div class="grid w-full grid-cols-7">
                                 <div class="flex flex-col col-span-5 text-xs">
                                     <span class="flex flex-row items-center space-x-2 dark:text-butterscotch-500 text-navy-500">
@@ -30,8 +30,13 @@
                             </div>
                             <div class="flex flex-col mt-4">
                                 <h3 class="h4">Details</h3>
-                                <p class="italic opacity-50" v-if="item.standard">Standard order for this supplier.</p>
-                                <div class="content" v-html="item.details" />
+                                <template v-if="item.details || item.standard">
+                                    <p class="italic opacity-50" v-if="item.standard">Standard order for this supplier.</p>
+                                    <div class="content" v-html="item.details" />
+                                </template>
+                                <p class="italic opacity-50" v-else>
+                                    No details
+                                </p>
                             </div>
                             <div v-if="item.notes" class="flex flex-col mt-4">
                                 <h3 class="h4">Notes</h3>
