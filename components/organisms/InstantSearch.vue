542d24844cc4f9a383525bfabb181f73ee3d7e73
Dan Farrow <dan@zellement.com>
2023-08-10 23:39:45 +0100

    update instant search

--- components/organisms/InstantSearch.vue
+++ components/organisms/InstantSearch.vue
@@ -1,18 +1,29 @@
 <template>
     <div v-if="uiStore.showSearchResults" class="fixed inset-0 z-50 flex bg-butterscotch dark:bg-navy-500">
 
-        <div class="flex flex-row mx-auto">
-            <div class="flex">
-                <input class="my-auto" name="site-search" id="site-search" placeholder="Search..." />
+        <button class="fixed top-2 right-2" @click="uiStore.toggleSearchResults">
+
+            <Icon name="ri:close-circle-line" class="w-8 h-8 transition-all duration-300 hover:rotate-90" />
+
+        </button>
+
+        <div class="flex flex-col w-full gap-8 mx-auto md:grid md:grid-cols-2">
+
+            <div class="flex py-8 md:justify-center">
+                <input v-model="searchInput" class="w-full max-w-[20rem] p-2 my-auto rounded dark:bg-navy-300" name="site-search" id="site-search" placeholder="Search..." />
             </div>
 
-            <div class="flex h-full">
+            <div class="flex h-screen overflow-hidden">
 
-                <ul class="h-full my-auto overflow-y-auto">
+                <ul class="flex flex-col overflow-y-auto md:justify-center ">
 
-                    <li v-for="item in data.allArticles" :key="item.id">
+                    <li v-for="item in filteredItems" :key="item.id">
 
-                        {{ item.title }}
+                        <nuxt-link @click="uiStore.toggleSearchResults" :to="`/article/${item.slug}`"
+                                   class="inline-flex flex-row items-end self-start generic-link">
+                            <span>{{ item.title }}</span>
+                            <span class="ml-4 text-2xs opacity-60">{{ dateConverter(item._updatedAt) }}</span>
+                        </nuxt-link>
 
                     </li>
                 </ul>
@@ -26,19 +37,32 @@
 <script setup>
 
 import { useUiStore } from '@/stores/ui'
+import { dateConverter } from '@/scripts/helpers'
 
 const uiStore = useUiStore()
 
+const searchInput = ref()
+
 const QUERY = `
     query MyQuery {
-       allArticles {
+       allArticles(orderBy: title_ASC) {
             id
             title
             slug
+            _updatedAt
         }
     }
 `
 
 const { data } = await useGraphqlQuery({ query: QUERY })
+
+const filteredItems = computed(() => {
+    if (searchInput.value) {
+        console.log(searchInput.value)
+        return data.value.allArticles.filter(item => {
+            return item.title.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1
+        })
+    } return data.value.allArticles
+})
 
 </script>
