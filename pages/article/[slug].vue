3f2865108914561210d8ca022c8d5737eaa0c0fc
Dan Farrow <dan@zellement.com>
2023-08-11 21:22:12 +0100

    add downloads

--- pages/article/[slug].vue
+++ pages/article/[slug].vue
@@ -1,21 +1,59 @@
 <template>
-    <div class="container">
-        <p>Last updated: {{ dateConverter(data.article._updatedAt) }}</p>
-        <h1 class="h1">
-            {{ data.article.title }}
-        </h1>
-        <h2 class="h2">
-            {{ data.article.subtitle }}
-        </h2>
-
-        <div
-            v-for="content in articleData.articleContent"
-            :key="content.id"
-        >
-            <div v-html="content.text" />
+    <div class="container flex flex-col items-center space-y-8">
+        <div class="flex flex-col items-center p-8 text-center shadow-xl bg-seashell/10 rounded-xl">
+            <p class="text-xs">
+                Last updated: {{ dateConverter(data.article._updatedAt) }}
+            </p>
+            <h1 class="h1">
+                {{ data.article.title }}
+            </h1>
+            <h2 class="h2 text-tuscany">
+                {{ data.article.subtitle }}
+            </h2>
         </div>
-
-        <!-- {{ articleData }} -->
+        <template v-if="articleData.articleContent">
+            <div
+                v-for="content in articleData.articleContent"
+                :key="content.id"
+            >
+                <div
+                    v-if="content._modelApiKey === 'text_block'"
+                    class="max-w-screen-md mx-auto content"
+                    v-html="content.text"
+                />
+                <div
+                    v-if="content._modelApiKey === 'image_block'"
+                >
+                    <div
+                        :class="content.fullWidth ? 'w-full' : 'max-w-screen-md'"
+                    >
+                        <img
+                            v-for="image in content.images"
+                            :key="image.id"
+                            :src="image.url"
+                        >
+                    </div>
+                </div>
+                <div
+                    v-if="content._modelApiKey === 'download_block'"
+                >
+                    <ul class="flex flex-col space-y-2">
+                        <li
+                            v-for="pdf in content.pdfs"
+                            :key="pdf.id"
+                            class="flex button"
+                        >
+                            <a
+                                :href="pdf.url"
+                                target="_blank"
+                            >
+                                {{ pdf.title }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -29,9 +67,36 @@ const QUERY = `query ArticleQuery ($slug: String!) {
             subtitle
             title
             articleContent {
+                ... on DownloadBlockRecord {
+                    id
+                    _modelApiKey
+                    pdfs {
+                        url
+                        title
+                    }
+            }
+                ... on ImageBlockRecord {
+                    id
+                    _modelApiKey
+                    fullWidth
+                    images {
+                        url
+                        id
+                    }
+                }
                 ... on TextBlockRecord {
                     id
                     text
+                    _modelApiKey
+                }
+                ... on VideoBlockRecord {
+                    id
+                    _modelApiKey
+                    video {
+                        url
+                        width
+                        height
+                    }
                 }
             }
         }
