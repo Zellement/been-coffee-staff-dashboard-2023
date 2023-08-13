1d4bc0d188b159d09ddc793012752c733f48f6a6
Dan Farrow <dan@zellement.com>
2023-08-13 14:28:34 +0100

    try mc

---
+++ components/organisms/MailchimpShots.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>hleo</div>
+</template>
+
+<script setup>
+// import mailchimp from '@mailchimp/mailchimp_marketing'
+// const mailchimp = require('@mailchimp/mailchimp_marketing')
+
+// import { createRequire } from 'node:module'
+// const require = createRequire(import.meta.url)
+
+// const mailchimp = require('@mailchimp/mailchimp_marketing')
+
+// const runtime = useRuntimeConfig()
+
+// mailchimp.setConfig({
+//     apiKey: runtime.public.MC_KEY,
+//     server: runtime.public.MC_SERVER
+// })
+
+// async function callPing () {
+//     const response = await mailchimp.ping.get()
+//     console.log(response)
+// }
+
+// callPing()
+</script>
---
+++ pages/mailchimp.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="relative">
+        <div class="flex flex-col">
+            <mailchimp-shots />
+        </div>
+    </div>
+</template>
