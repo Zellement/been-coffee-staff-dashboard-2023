f6369219874e8728d13fb6629e940cb05666ee51
Dan Farrow <dan@zellement.com>
2023-08-13 22:07:09 +0100

    format all docs

--- components/atoms/SiteLogo.vue
+++ components/atoms/SiteLogo.vue
@@ -1,5 +1,11 @@
 <template>
-    <nuxt-link to="/" class="flex">
-        <img class="w-6 h-6" src="@/assets/images/been-staff-dashboard.png">
+    <nuxt-link
+        to="/"
+        class="flex"
+    >
+        <img
+            class="w-6 h-6"
+            src="@/assets/images/been-staff-dashboard.png"
+        >
     </nuxt-link>
 </template>
