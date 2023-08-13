790a380a57f9700200d3a28f5d9f90a176654b2e
Dan Farrow <dan@zellement.com>
2023-08-13 22:25:24 +0100

    more style changes

--- components/organisms/InstantSearch.vue
+++ components/organisms/InstantSearch.vue
@@ -14,48 +14,23 @@
         </button>
 
         <div class="flex flex-col w-full mx-auto mt-8 lg:m-0 md:grid md:grid-cols-2">
-            <div class="flex flex-col items-center gap-8 p-8 md:justify-center">
+            <div class="flex p-4 md:col-span-2">
                 <input
                     id="site-search"
                     v-model="searchInput"
-                    class="w-full max-w-[20rem] p-2 rounded dark:bg-navy-300"
+                    class="w-full max-w-[20rem] mx-auto p-2 rounded dark:bg-navy-300"
                     name="site-search"
-                    placeholder="Search..."
+                    placeholder="Search articles..."
                 >
-                <div
-                    v-if="cats"
-                    class="flex flex-col w-full  max-w-[20rem]"
-                >
-                    <h2 class="mb-2 h4">
-                        Categories
-                    </h2>
-                    <ul
-                        class="flex flex-col overflow-y-auto md:justify-center "
-                    >
-                        <li
-                            v-for="item in cats"
-                            :key="item.id"
-                        >
-                            <nuxt-link
-                                :to="`/category/${item.slug}`"
-                                class="inline-flex flex-row items-end self-start generic-link"
-                                @click="uiStore.toggleSearchResults"
-                            >
-                                <span>{{ item.title }}</span>
-                            </nuxt-link>
-                        </li>
-                    </ul>
-                </div>
             </div>
-
-            <div class="flex flex-col h-screen p-8 overflow-hidden lg:py-16">
-                <div class="flex flex-row justify-between">
+            <div class="flex flex-col h-screen p-4 overflow-hidden xl:p-16 xl:pt-0">
+                <div class="flex flex-row items-center justify-between">
                     <h2 class="mb-2 h4">
                         Article
                     </h2>
-                    <span>Last updated</span>
+                    <span class="text-xs">Last updated</span>
                 </div>
-                <ul class="relative z-10 flex flex-col overflow-y-scroll md:justify-center ">
+                <ul class="relative z-10 flex flex-col pb-8 overflow-y-scroll md:justify-center">
                     <li
                         v-for="item in filteredItems"
                         :key="item.id"
@@ -63,11 +38,36 @@
                     >
                         <nuxt-link
                             :to="`/article/${item.slug}`"
-                            class="flex flex-row items-center self-start justify-between w-full py-1 shadow-sm"
+                            class="flex flex-row items-center self-start justify-between w-full py-1 transition-all duration-300 border-b border-transparent shadow-sm hover:border-current"
                             @click="uiStore.toggleSearchResults"
                         >
                             <span>{{ item.title }}</span>
                             <span class="text-right whitespace-nowrap text-2xs opacity-60">{{ dateConverter(item._updatedAt) }}</span>
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div>
+
+            <div
+                v-if="cats"
+                class="flex flex-col w-full p-4 max-w-[20rem] xl:p-16 xl:pt-0"
+            >
+                <h2 class="mb-2 h4">
+                    Categories
+                </h2>
+                <ul
+                    class="flex flex-col overflow-y-auto md:justify-center "
+                >
+                    <li
+                        v-for="item in cats"
+                        :key="item.id"
+                    >
+                        <nuxt-link
+                            :to="`/category/${item.slug}`"
+                            class="inline-flex flex-row items-end self-start transition-all duration-300 border-b border-transparent hover:border-current"
+                            @click="uiStore.toggleSearchResults"
+                        >
+                            <span>{{ item.title }}</span>
                         </nuxt-link>
                     </li>
                 </ul>
@@ -113,7 +113,6 @@ const { data } = await useGraphqlQuery({ query: QUERY })
 
 const filteredItems = computed(() => {
     if (searchInput.value) {
-        console.log(searchInput.value)
         return data.value.allArticles.filter(item => {
             return item.title.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1
         })
