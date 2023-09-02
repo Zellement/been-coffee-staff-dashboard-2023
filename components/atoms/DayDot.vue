ae7bc3f378d486df912cb5726cf747084f5d598d
Dan Farrow <dan@zellement.com>
2023-09-02 15:16:45 +0100

    add in day dot helper

---
+++ components/atoms/DayDot.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="flex flex-col items-center gap-4">
+        <h2 class="h4">
+            <template v-if="props.title">
+                {{ props.title }}
+            </template>
+            <template v-else>
+                Today: <span>{{ dateConverterWithDayNoYear(props.date) }}</span>
+            </template>
+        </h2>
+        <div class="flex flex-col">
+            <div class="flex flex-col items-center w-24 h-24 overflow-hidden bg-white border-2 border-white rounded-full">
+                <span
+                    class="flex w-full text-xl basis-1/2 font-riverside"
+                    :class="dayDotConverter[getDayNumeric(props.date)].classes"
+                >
+                    <span class="m-auto">{{ getDayAlpha(props.date) }}</span>
+                </span>
+                <span
+                    class="flex w-full text-xl basis-1/2 font-riverside"
+                >
+                    <span class="m-auto text-black -translate-y-0.5">
+                        {{ getDate(props.date) }} / {{ getMonth(props.date) }}
+                    </span>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { dateConverterWithDayNoYear } from '@/scripts/helpers'
+
+const props = defineProps({
+    date: {
+        type: Date,
+        required: true
+    },
+    title: {
+        type: String,
+        required: false,
+        default: null
+    }
+})
+
+const getDayNumeric = (date) => {
+    return date.getDay()
+}
+
+const getDayAlpha = (date) => {
+    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    return dayNames[date.getDay()]
+}
+
+const getDate = (date) => {
+    return date.getDate()
+}
+
+const getMonth = (date) => {
+    return date.getMonth() + 1
+}
+
+// var today = new Date();
+// var plus3days = 6;
+// var result = today.setDate(today.getDate() + plus3days);
+// console.log(new Date(result))
+
+const dayDotConverter = [
+    {
+        classes: 'bg-black text-white'
+    },
+    {
+        classes: 'bg-blue-400 text-white'
+    },
+    {
+        classes: 'bg-yellow-500 text-black'
+    },
+    {
+        classes: 'bg-red-500 text-white'
+    },
+    {
+        classes: 'bg-yellow-800 text-white'
+    },
+    {
+        classes: 'bg-green-500 text-white'
+    },
+    {
+        classes: 'bg-orange-500 text-white'
+    }
+]
+
+</script>
---
+++ pages/day-dot-helper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="flex flex-col w-full max-w-screen-md px-10 pt-0 pb-32 mx-auto">
+        <h1 class="mb-8 h1">
+            Day Dot Helper
+        </h1>
+
+        <div class="grid grid-cols-2 space-y-4">
+            <day-dot
+                :date="today"
+                class="col-span-2"
+            />
+            <day-dot
+                :date="plus3days"
+                title="+ 3 Days"
+            />
+            <day-dot
+                :date="plus5days"
+                title="+ 5 Days"
+            />
+            <day-dot
+                :date="plus7days"
+                title="+ 7 Days"
+            />
+            <day-dot
+                :date="plus14days"
+                title="+ 14 Days"
+            />
+            <day-dot
+                :date="plus21days"
+                title="+ 21 Days"
+            />
+            <day-dot
+                :date="plus28days"
+                title="+ 28 Days"
+            />
+        </div>
+    </div>
+</template>
+<script setup>
+
+const today = new Date()
+const plus3days = new Date(new Date().getTime() + (3 * 24 * 60 * 60 * 1000))
+const plus5days = new Date(new Date().getTime() + (5 * 24 * 60 * 60 * 1000))
+const plus7days = new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000))
+const plus14days = new Date(new Date().getTime() + (14 * 24 * 60 * 60 * 1000))
+const plus21days = new Date(new Date().getTime() + (21 * 24 * 60 * 60 * 1000))
+const plus28days = new Date(new Date().getTime() + (28 * 24 * 60 * 60 * 1000))
+
+</script>
