1ffd6036dafb126d1562c737b9c383757d83701b
Dan Farrow <dan@zellement.com>
2023-12-10 15:32:47 +0000

    pointer events none on santa

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
         />
         <div
             v-if="isXmasTheme"
-            class="absolute top-0 right-0 flex w-1/2 h-auto overflow-hidden "
+            class="absolute top-0 right-0 flex w-1/2 h-auto overflow-hidden pointer-events-none "
         >
             <lottie-swinging-santa class="-translate-y-10 translate-x-1/4" />
         </div>
