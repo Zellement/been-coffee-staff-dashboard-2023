c3eb7c3534e2701be33c0629b26c18655e357721
Dan Farrow <dan@zellement.com>
2023-12-10 14:15:44 +0000

    add home btn

--- components/atoms/SiteLogo.vue
+++ components/atoms/SiteLogo.vue
@@ -1,15 +1,15 @@
 <template>
     <nuxt-link
         to="/"
-        class="flex items-center gap-2"
+        class="flex items-center gap-2 group"
     >
         <img
-            class="w-6 h-6"
+            class="w-8 h-8"
             src="@/assets/images/been-staff-dashboard.png"
         >
         <div
             v-if="showHomeIcon"
-            class="flex flex-row items-center gap-1"
+            class="flex flex-row items-center gap-1 group-hover:text-butterscotch-500"
         >
             <Icon
                 name="material-symbols:house"
