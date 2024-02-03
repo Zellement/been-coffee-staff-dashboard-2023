74ef8a6d5912cee46833a9dd56a785456684fbc6
Dan Farrow <dan@zellement.com>
2024-02-03 10:12:05 +0000

    switch to h1 class

--- components/molecules/BirthdaysAnniversaries.vue
+++ components/molecules/BirthdaysAnniversaries.vue
@@ -4,7 +4,7 @@
         class="relative"
     >
         <div class="px-2 overflow-hidden md:px-4">
-            <h2 class="mb-6 text-3xl font-riverside">
+            <h2 class="mb-8 h1">
                 Celebrations today!
             </h2>
             <div class="grid grid-cols-2 gap-4 lg:grid-cols-3">
