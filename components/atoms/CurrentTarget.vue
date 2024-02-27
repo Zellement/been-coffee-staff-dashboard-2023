0ec9724214c8b82af9d2f7cd691bd2a87ad1e085
Dan Farrow <dan@zellement.com>
2024-02-27 18:34:33 +0000

    close nav on click of target day bonus link

--- components/atoms/CurrentTarget.vue
+++ components/atoms/CurrentTarget.vue
@@ -2,6 +2,7 @@
     <nuxt-link
         class="fixed z-50 flex flex-col items-center p-1 text-white rounded text-2xs bottom-16 right-1 bg-tuscany"
         to="/article/target-day-bonus"
+        @click="uiStore.toggleSearchResults(false)"
     >
         <p class="leading-none">
             Next Target
@@ -12,3 +13,11 @@
         <span class="leading-none underline">What is this?</span>
     </nuxt-link>
 </template>
+
+<script setup>
+
+import { useUiStore } from '@/stores/ui'
+
+const uiStore = useUiStore()
+
+</script>
