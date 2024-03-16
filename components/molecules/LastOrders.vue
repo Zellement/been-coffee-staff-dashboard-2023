14e17054e458d3e56d2b493d9924dcc1ce0c7f7e
Dan Farrow <dan@zellement.com>
2024-03-16 10:50:54 +0000

    add portable text

--- components/molecules/LastOrders.vue
+++ components/molecules/LastOrders.vue
@@ -14,10 +14,10 @@
                         class="flex relative flex-col w-3/4 p-4 shadow-lg rounded-xl  min-w-[300px] bg-seashell-400 dark:bg-navy-400"
                     >
                         <div class="absolute top-0 right-0 uppercase -translate-y-1/2 bg-white text-3xs px-2 dark:text-navy-50 dark:bg-navy-400 text-gray-400 rounded-tl-lg py-0.5 tracking-wider">
-                            {{ item.supplier.name }}
+                            {{ item.supplier.title }}
                         </div>
                         <div class="flex flex-col w-full">
-                            <div class="flex justify-between h-16 p-1 mb-4 overflow-hidden bg-white border border-gray-200 rounded-full shadow-lg">
+                            <div class="flex justify-between h-16 p-1 mb-2 overflow-hidden bg-white border border-gray-200 rounded-full shadow-lg">
                                 <img
                                     :src="$urlFor(item.supplier.logo).height(50).fit('clip').url()"
                                     class="w-auto my-auto max-w-[60%] pl-4"
@@ -25,14 +25,22 @@
                                     loading="lazy"
                                 >
                                 <img
-                                    :src="$urlFor(item.orderedBy.image).width(60).height(60).url()"
-                                    class="flex-shrink-0 rounded-full"
-                                    width="60"
-                                    height="60"
+                                    :src="$urlFor(item.orderedBy.image).width(54).height(54).url()"
+                                    class="rounded-full aspect-square"
+                                    width="54"
+                                    height="54"
                                     loading="lazy"
                                 >
                             </div>
-                            <div class="flex flex-col text-sm lg:text-xs">
+                            <div
+                                class="mb-4 text-xs italic text-center opacity-50"
+                            >
+                                <p>
+                                    Ordered by {{ getOrderedBy(item) }} on
+                                    {{ dateConverterWithDay(item.orderDate) }}
+                                </p>
+                            </div>
+                            <div class="flex flex-col text-sm">
                                 <span class="flex flex-row items-center justify-center space-x-2 dark:text-butterscotch-500 text-navy-500">
                                     <Icon
                                         class="w-6 h-6 p-1 rounded-full bg-butterscotch-500 dark:bg-navy-700 "
@@ -44,49 +52,25 @@
                                 </span>
                             </div>
                         </div>
-                        <div
-                            class="mt-4 text-xs italic text-center opacity-50"
-                        >
-                            <p>
-                                Ordered by {{ getOrderedBy(item) }} on
-                                {{ dateConverterWithDay(item.orderDate) }}
-                            </p>
-                        </div>
-                        <div class="flex flex-col mt-4">
-                            <h3 class="h4">
-                                Details
-                            </h3>
-                            <template v-if="item.details || item.standard">
-                                <p
-                                    v-if="item.standard"
-                                    class="italic opacity-50"
-                                >
-                                    Standard order for this supplier
+                        <template v-if="item.usualOrder">
+                            <div class="flex flex-col mt-4">
+                                <p class="italic text-center opacity-50">
+                                    Usual order for this supplier.
                                 </p>
-                                <div
-                                    class="content"
-                                    v-html="item.details"
-                                />
-                            </template>
-                            <p
-                                v-else
-                                class="italic opacity-50"
-                            >
-                                No details
-                            </p>
-                        </div>
-                        <div
-                            v-if="item.notes"
-                            class="flex flex-col mt-4"
-                        >
-                            <h3 class="h4">
-                                Notes
-                            </h3>
-                            <div
-                                class="content"
-                                v-html="item.notes"
-                            />
-                        </div>
+                            </div>
+                        </template>
+                        <template v-if="item.orderDetails">
+                            <div class="flex flex-col mt-4">
+                                <h3 class="h4">
+                                    Details
+                                </h3>
+                                <div class="content">
+                                    <PortableText
+                                        :value="item.orderDetails"
+                                    />
+                                </div>
+                            </div>
+                        </template>
                     </div>
                 </div>
             </div>
@@ -96,6 +80,7 @@
 
 <script setup>
 import { dateConverterWithDay } from '@/scripts/helpers'
+import { PortableText } from '@portabletext/vue'
 
 const query = groq`
 *[_type == "order"][0..14] | order(expectedDeliveryDate asc){
