a0b7b7488c1f0e1ed365442e1aa342c78ae5875f
Dan Farrow <dan@zellement.com>
2024-03-16 11:26:35 +0000

    get standing orders in

--- components/molecules/StandingOrders.vue
+++ components/molecules/StandingOrders.vue
@@ -9,58 +9,42 @@
             <div class="w-full py-8 overflow-x-scroll ">
                 <div class="flex flex-row w-full space-x-4">
                     <div
-                        v-for="item in data.allStandingOrders"
-                        :key="item.id"
+                        v-for="item in standingOrders"
+                        :key="item._id"
                         class="flex relative flex-col w-3/4 p-4 shadow-lg rounded-xl md:w-auto min-w-[300px] sm:w-2/5 bg-seashell-400 dark:bg-navy-400"
                     >
-                        <div class="absolute top-0 right-0 uppercase -translate-y-1/2 bg-white text-3xs px-2 dark:text-navy-50 dark:bg-navy-400 text-gray-400 rounded-tl-lg py-0.5 tracking-wider">
-                            {{ item.supplier.name }}
+                        <div class="absolute top-0 left-0 uppercase -translate-y-1/2 bg-white text-3xs px-2 dark:text-navy-50 dark:bg-navy-400 text-gray-400 rounded-tl-lg py-0.5 tracking-wider">
+                            {{ item.supplier.title }}
                         </div>
-                        <div class="grid w-full grid-cols-7 gap-2">
-                            <div class="flex flex-col col-span-5 text-xs">
-                                <span class="flex flex-row items-center my-auto space-x-2 dark:text-butterscotch-500 text-navy-500">
-                                    <Icon
-                                        class="w-5 h-5 p-1 rounded-full bg-butterscotch-500 dark:bg-navy-700 "
-                                        name="clarity:truck-solid"
-                                    />
-                                    <span>{{ item.deliverySchedule }}
-                                    </span>
-                                </span>
-                            </div>
-                            <div class="flex col-span-2 p-1 overflow-hidden bg-white rounded-full">
-                                <img
-                                    class="w-auto h-auto my-auto ml-auto dark:m-auto max-h-12"
-                                    :src="item.supplier.logo.url"
-                                >
-                            </div>
-                        </div>
-                        <div class="flex flex-col mt-4">
-                            <h3 class="h4">
-                                Details
-                            </h3>
-                            <p
-                                v-if="item.standard"
-                                class="italic opacity-50"
+                        <div class="flex col-span-2 px-1 py-2 mb-4 overflow-hidden bg-white rounded-full">
+                            <img
+                                class="w-auto h-auto m-auto max-h-12"
+                                :src="$urlFor(item.supplier.logo).height(60).url()"
+                                height="60"
                             >
-                                Standard order for this supplier.
-                            </p>
-                            <div
-                                class="content"
-                                v-html="item.details"
-                            />
                         </div>
-                        <div
-                            v-if="item.notes"
-                            class="flex flex-col mt-4"
-                        >
-                            <h3 class="h4">
-                                Notes
-                            </h3>
-                            <div
-                                class="content"
-                                v-html="item.notes"
-                            />
+                        <div class="flex flex-col col-span-5 text-xs">
+                            <span class="flex flex-row items-center my-auto space-x-2 dark:text-butterscotch-500 text-navy-500">
+                                <Icon
+                                    class="w-5 h-5 p-1 rounded-full bg-butterscotch-500 dark:bg-navy-700 "
+                                    name="clarity:truck-solid"
+                                />
+                                <span>{{ item.frequency }}
+                                </span>
+                            </span>
                         </div>
+                        <template v-if="item.standingOrderDetails">
+                            <div class="flex flex-col mt-4">
+                                <h3 class="h4">
+                                    Details
+                                </h3>
+                                <div class="content">
+                                    <PortableText
+                                        :value="item.standingOrderDetails"
+                                    />
+                                </div>
+                            </div>
+                        </template>
                     </div>
                 </div>
             </div>
@@ -69,22 +53,27 @@
 </template>
 
 <script setup>
-const QUERY = `
-query {
-    allStandingOrders {
-    id
-    deliverySchedule
-    details
-     supplier {
-        logo {
-            url
-        }
-        name
-        }
-    }
+
+import { PortableText } from '@portabletext/vue'
+
+const query = groq`
+*[_type == "standingOrder"]{
+    supplier->{
+        logo,
+        title
+    },
+    _id,
+    frequency,
+    standingOrderDetails
 }
 `
 
-const { data } = await useGraphqlQuery({ query: QUERY })
+const sanity = useSanity()
+
+const { data } = await useAsyncData('standingOrders', () => sanity.fetch(query))
+
+const standingOrders = computed(() => {
+    return data.value
+})
 
 </script>
