2b7d185b3dc64f1eafa74540aa7b17318f99d5ba
Dan Farrow <dan@zellement.com>
2024-03-17 16:29:13 +0000

    add related files

--- pages/article/[slug].vue
+++ pages/article/[slug].vue
@@ -1,7 +1,7 @@
 <template>
     <div
         v-if="articleData"
-        class="container flex flex-col items-center pb-20 mb-16 space-y-8"
+        class="container flex flex-col items-center pb-20 mb-16"
     >
         <content-hero
             :title="articleData.title"
@@ -9,17 +9,81 @@
             :date="articleData.publishedAt"
             :categories="articleData.categories"
         />
-        <div class="content">
-            <PortableText
-                :value="articleData.content"
-                :components="myPortableTextComponents"
-            />
+        <div class="flex flex-col">
+            <div class="grid w-full grid-cols-2 py-4">
+                <p
+                    v-if="articleData._updatedAt"
+                    class="text-xs opacity-50 font-krete"
+                >
+                    Last updated: {{ dateConverter(articleData._updatedAt) }}
+                </p>
+                <ul
+                    v-if="articleData.categories"
+                    class="flex flex-row flex-wrap gap-2 ml-auto"
+                >
+                    <li
+                        v-for="category in articleData.categories"
+                        :key="category._id"
+                        class="text-xs font-krete text-tuscany"
+                    >
+                        <nuxt-link
+                            class="underline underline-offset-2"
+                            :to="`/category/${category.slug.current}`"
+                        >
+                            {{ category.title }}
+                        </nuxt-link>
+                    </li>
+                </ul>
+                <div
+                    v-if="articleData.files"
+                    class="py-8 col-span-full"
+                >
+                    <h2 class="font-krete">
+                        <Icon
+                            name="material-symbols-light:file-copy-rounded"
+                            class="w-4 h-4 "
+                        />
+                        Related files &amp; downloads
+                    </h2>
+                    <div class="w-full pb-4 overflow-x-scroll">
+                        <ul
+                            class="flex w-full gap-4"
+                        >
+                            <li
+                                v-for="file in articleData.files"
+                                :key="file._key"
+                                class="flex "
+                            >
+                                <a
+                                    :href="file.asset.url"
+                                    target="_blank"
+                                    class="w-full relative h-full min-w-[270px] pl-4 py-1 pr-2 text-xs font-krete shadow dark:shadow-navy-400 text-tuscany rounded"
+                                >
+                                    <Icon
+                                        name="material-symbols-light:file-copy-rounded"
+                                        class="absolute left-0 w-3 h-3 opacity-50 top-3"
+                                    />
+                                    {{ file.asset.originalFilename }}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <PortableText
+                    :value="articleData.content"
+                    :components="myPortableTextComponents"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { PortableText } from '@portabletext/vue'
+import { dateConverter } from '@/scripts/helpers'
+// import { getFileAsset } from '@sanity/asset-utils'
 
 const route = useRoute()
 const query = groq`*[_type == "article" && slug.current == '${route.params.slug}'] [0]{
@@ -28,7 +92,13 @@ const query = groq`*[_type == "article" && slug.current == '${route.params.slug}
     publishedAt,
     _updatedAt,
     content,
-    files,
+    files[]{
+      _key,
+      asset->{
+        url,
+        originalFilename
+      }
+    },
     sticky,
     categories[]->{
         _id,
@@ -40,6 +110,14 @@ const query = groq`*[_type == "article" && slug.current == '${route.params.slug}
 const sanity = useSanity()
 
 const { data } = await useAsyncData('singleArticle', () => sanity.fetch(query))
+
+console.log(data)
+
+// const getAsset = (asset) => {
+//     console.log('asset', asset)
+//     // const file = getFileAsset(asset, { projectId: 'mxklvbih', dataset: 'production' })
+//     // console.log('file', file)
+// }
 
 const articleData = computed(() => {
     return data.value
