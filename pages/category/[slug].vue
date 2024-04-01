5c5fc9f832a9e6597cfb56f1a4d94d2a1eb76cd2
Dan Farrow <dan@zellement.com>
2024-04-01 19:10:15 +0100

    fix cat page

--- pages/category/[slug].vue
+++ pages/category/[slug].vue
@@ -1,21 +1,20 @@
 <template>
     <div
-        class="container flex flex-col items-center space-y-8"
+        class="container flex flex-col space-y-8"
     >
-        <!-- <content-hero
-            v-if="categoryData"
-            :title="categoryData.title"
+        <content-hero
+            :title="catTitle"
             subtitle="Category"
         />
         <div
-            v-if="allArticles"
-            class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4"
+            v-if="data"
+            class="grid grid-cols-1 gap-4 pb-16 md:grid-cols-2 xl:grid-cols-4"
         >
             <nuxt-link
-                v-for="article in allArticles"
-                :key="article.id"
-                :to="`/article/${article.slug}`"
-                class="flex flex-col p-4 transition-all duration-300 rounded bg-butterscotch-500 dark:bg-navy-600 bg-opacity-10 hover:bg-opacity-80 dark:hover:text-butterscotch-200"
+                v-for="article in data"
+                :key="article._id"
+                :to="`/article/${article.slug.current}`"
+                class="flex flex-col w-full p-4 transition-all duration-300 rounded bg-butterscotch-500 dark:bg-navy-600 bg-opacity-10 hover:bg-opacity-80 dark:hover:text-butterscotch-200"
             >
                 <h2 class="h4">
                     {{ article.title }}
@@ -28,43 +27,37 @@
                         class="w-4 h-4"
                         name="ri:calendar-2-fill"
                     />
-                    <span>{{ dateConverter(article._updatedAt) }}</span></span>
+                    <span>{{ dateConverter(article.publishedAt) }}</span></span>
             </nuxt-link>
-        </div> -->
+        </div>
     </div>
 </template>
 
 <script setup>
-// import { dateConverter } from '@/scripts/helpers'
+import { dateConverter } from '@/scripts/helpers'
+const { toPascalCase } = useStringUtils()
+const route = useRoute()
 
-// const QUERY = `query CatQuery ($slug: String!) {
-//     category(filter: {slug: {eq: $slug}})  {
-//         id
-//         title
-//         slug
-//         _allReferencingArticles {
-//             id
-//             slug
-//             title
-//             subtitle
-//             _updatedAt
-//         }
-//     }
-// }
-// `
-// const route = useRoute()
-// const { data } = await useGraphqlQuery({ query: QUERY, variables: { slug: route.params.slug } })
+const query = groq`*[_type == "article" && references(*[ slug.current == "${route.params.slug}"]._id)]{
+  title,
+  subtitle,
+    slug,
+    _id,
+    publishedAt,
+    categories[]->{
+      slug
+    }
+}
+`
 
-// const categoryData = computed(() => {
-//     return data?.value?.category
-// })
+const sanity = useSanity()
 
-// const allArticles = computed(() => {
-//     return data?.value?.category._allReferencingArticles
-// })
+const { data } = await useAsyncData('catArticles', () => sanity.fetch(query))
 
-// useHead({
-//     title: categoryData.value?.title
-// })
+const catTitle = computed(() => toPascalCase(route.params.slug))
+
+useHead({
+    title: catTitle.value
+})
 
 </script>
