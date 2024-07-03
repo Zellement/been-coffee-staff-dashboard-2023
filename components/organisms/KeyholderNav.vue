520d6610c60bae16fabb948efdcd1d45ca3e6773
Dan Farrow <dan@zellement.com>
2024-07-03 22:36:29 +0100

    tweaks

--- components/organisms/KeyholderNav.vue
+++ components/organisms/KeyholderNav.vue
@@ -5,72 +5,83 @@
                 Keyholder Quick Items
             </h2>
 
-            <ul class="grid grid-cols-1 gap-2 md:grid-cols-2">
-                <li
-                    v-for="link in nav"
-                    :key="link.url"
-                    class="flex flex-col items-start gap-2 p-4 bg-white shadow-lg card"
-                >
-                    <div class="flex justify-between items-center w-full gap-4">
-                        <span class="flex items-center gap-2">
-                            <Icon
-                                :name="link.icon"
-                                class="w-4 h-5"
-                            />
-                            <h3 class="krete-title">{{ link.title }}</h3>
-                        </span>
-                        <component
-                            :is="link.component"
-                            class="self-center"
-                        />
-                    </div>
-                    <ul class="flex flex-row items-center gap-2 text-2xs">
+            <div
+                class="px-2 overflow-hidden md:px-4 xl:px-6"
+            >
+                <div class="w-full py-8 overflow-x-scroll ">
+                    <ul class="flex flex-row w-full">
                         <li
-                            v-for="subnav in link.subnav"
-                            :key="subnav.url"
+                            v-for="link in nav"
+                            :key="link.url"
+                            class="flex flex-col  w-3/4 min-w-[300px] items-start gap-2 p-4 bg-white shadow-lg card mr-4 relative"
+                            :class="link.class"
                         >
-                            <nuxt-link
-                                v-if="subnav.url"
-                                :to="subnav.url"
-                                :target="subnav.blank ? '_blank' : null"
-                                class="flex flex-row button items-center gap-1 p-0.5 px-1"
-                            >
-                                {{ subnav.title }}
+                            <div class="flex justify-between items-center w-full gap-4">
+                                <span class="flex items-center gap-2 relative">
+                                    <!-- <Icon
+                                        :name="link.icon"
+                                        class="w-4 h-5 absolute top-0 left-0"
+                                    /> -->
+                                    <div class="flex flex-col">
+                                        <span class="text-2xs">{{ link.brow }}</span>
+                                        <h3 class="font-krete">{{ link.title }}</h3>
+                                    </div>
+                                </span>
+                                <div class=" absolute top-0 right-0 -mt-2 mr-1">
+                                    <component
+                                        :is="link.component"
+                                        class="self-center"
+                                    />
+                                </div>
+                            </div>
+                            <ul class="flex flex-row items-center gap-2 text-2xs mt-auto">
+                                <li
+                                    v-for="subnav in link.subnav"
+                                    :key="subnav.url"
+                                >
+                                    <nuxt-link
+                                        v-if="subnav.url"
+                                        :to="subnav.url"
+                                        :target="subnav.blank ? '_blank' : null"
+                                        class="flex flex-row button items-center gap-1 p-0.5 px-1"
+                                    >
+                                        {{ subnav.title }}
 
-                                <Icon
-                                    v-if="subnav.blank"
-                                    name="iconamoon:link-external-fill"
-                                    class="w-3 h-3 transition-all duration-300 hover:rotate-90"
-                                />
-                            </nuxt-link>
+                                        <Icon
+                                            v-if="subnav.blank"
+                                            name="iconamoon:link-external-fill"
+                                            class="w-3 h-3 transition-all duration-300 hover:rotate-90"
+                                        />
+                                    </nuxt-link>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import BooleanCheckDailyCashBreakdown from '@/components/atoms/BooleanCheckDailyCashBreakdown.vue'
+import { useSupabaseStore } from '@/stores/supabase'
+
+const supabaseStore = useSupabaseStore()
+
+const temperaturesOrder = computed(() => {
+    return supabaseStore.daily_temperatures ? 'order-last ' : 'order-first'
+})
 
 const nav = computed(() => {
     return [
-        {
-            title: 'Daily Cash Breakdown',
-            icon: 'streamline:money-cash-coins-stack-accounting-billing-payment-stack-cash-coins-currency-money-finance',
-            subnav: [
-                {
-                    title: 'Form',
-                    url: '/forms/daily-cash-breakdown'
 
-                }
-            ]
-        },
         {
-            title: 'Daily Temperature Logs',
-            icon: 'mingcute:low-temperature-line',
+            brow: 'Daily',
+            title: 'Temperature Logs',
+            // icon: 'mingcute:low-temperature-line',
             component: BooleanCheckDailyCashBreakdown,
+            class: temperaturesOrder.value,
             subnav: [
                 {
                     title: 'Form',
@@ -81,6 +92,18 @@ const nav = computed(() => {
                     title: 'Responses',
                     url: 'https://docs.google.com/spreadsheets/d/1Bh5vjjW7wU8HaMsP7-R0M0XTlx1Ohd9uh25AK3ZZrIY/edit?usp=sharing',
                     blank: true
+                }
+            ]
+        },
+        {
+            brow: 'Daily',
+            title: 'Cash Breakdown',
+            // icon: 'streamline:money-cash-coins-stack-accounting-billing-payment-stack-cash-coins-currency-money-finance',
+            subnav: [
+                {
+                    title: 'Form',
+                    url: '/forms/daily-cash-breakdown'
+
                 }
             ]
         }
