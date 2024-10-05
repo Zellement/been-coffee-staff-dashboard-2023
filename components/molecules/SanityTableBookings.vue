85592b9fe0e8971c7d36a9b12675155d2b22e927
Dan Farrow <dan@zellement.com>
2024-10-05 07:56:29 +0100

    add people number

--- components/molecules/SanityTableBookings.vue
+++ components/molecules/SanityTableBookings.vue
@@ -19,9 +19,9 @@
                 >
                     <div
                         v-if="shortDateConverter(today) === shortDateConverter(booking.datetime)"
-                        class="bg-tuscany-500 flex-1 gap-2 py-2 px-4 flex flex-col text-white"
+                        class="bg-tuscany-500 flex-1 gap-2 py-2 px-4 grid grid-cols-3 text-white"
                     >
-                        <span class="text-3xl font-riverside flex items-center gap-4">
+                        <span class="text-2xl font-riverside flex items-center gap-4">
 
                             <Icon
                                 name="material-symbols:alarm"
@@ -29,7 +29,7 @@
                             />
 
                             {{ extractHourAndMinute(booking.datetime) }}</span>
-                        <span class="flex flex-row gap-4 items-start ">
+                        <span class="flex text-xl font-riverside items-center flex-row gap-4">
                             <Icon
                                 name="material-symbols:person"
                                 class="w-6 h-6 flex-shrink-0 text-butterscotch-500"
@@ -37,9 +37,17 @@
                             <span>{{ booking.name }}</span>
 
                         </span>
+                        <span class="flex text-xl font-riverside items-center flex-row gap-4">
+                            <Icon
+                                name="fluent:people-community-16-filled"
+                                class="w-6 h-6 flex-shrink-0 text-butterscotch-500"
+                            />
+                            <span>{{ booking.people }}</span>
+
+                        </span>
                         <span
                             v-if="booking.info"
-                            class="flex flex-row gap-4 items-start "
+                            class="flex col-span-full flex-row gap-4 items-start "
                         >
                             <Icon
                                 name="material-symbols:android-messages"
