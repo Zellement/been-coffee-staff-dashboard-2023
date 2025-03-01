0e1f12dc5f69473ebfae32c6380074ef39165b0d
Dan Farrow <dan@zellement.com>
2025-03-01 22:15:41 +0000

    minor

--- server/api/homebase-shifts.js
+++ server/api/homebase-shifts.js
@@ -1,13 +1,16 @@
 export default defineEventHandler(async (event) => {
     const query = getQuery(event)
     try {
-        const response = await fetch(`https://app.joinhomebase.com/api/public/locations/${process.env.HOMEBASE_LOCATION_WILLINGTON_ID}/shifts?start_date=${query.date}&end_date=${query.date}`, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${process.env.HOMEBASE_API_KEY}`,
-                Accept: 'application/vnd.homebase-v1+json'
+        const response = await fetch(
+            `https://app.joinhomebase.com/api/public/locations/${process.env.HOMEBASE_LOCATION_WILLINGTON_ID}/shifts?start_date=${query.date}&end_date=${query.date}`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${process.env.HOMEBASE_API_KEY}`,
+                    Accept: 'application/vnd.homebase-v1+json'
+                }
             }
-        })
+        )
 
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`)
@@ -16,7 +19,7 @@ export default defineEventHandler(async (event) => {
         const data = await response.json()
 
         return data
-    // Process the data as needed
+        // Process the data as needed
     } catch (error) {
         console.error('Failed to fetch Homebase locations:', error)
     }
