c5e4bc407de623d2c657c6f59c9110482bd915f2
Dan Farrow <dan@zellement.com>
2025-03-29 14:31:58 +0000

    refine find.

---
+++ components/molecules/WeatherData.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">{{ data }}</div>
+</template>
+
+<script lang="ts" setup>
+const data = ref(null)
+
+const fetchData = async () => {
+    data.value = useFetch('api/weather-api.js')
+}
+
+fetchData()
+</script>
